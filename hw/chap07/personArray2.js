0b007b6bcfec161a0355aa1e6ce2fb384c144753
Jungea <jungea2241@gmail.com>
2020-04-12 15:49:54 +0900

    hw_chap07

---
+++ hw/chap07/personArray2.js
@@ -0,0 +1,16 @@
+let persons1 = [];
+
+for (let i = 0; i < 10; ++i) {
+    persons1.push({
+        name: "홍길동",
+        age: 16 + i
+    });
+}
+
+let persons2 = []
+
+for (let i = 0; i < persons1.length; ++i) {
+    persons2.push(Object.assign({}, persons1[i]));
+}
+
+console.log(persons2);
